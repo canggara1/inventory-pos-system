478c4e1dd880f5ebd42ab89d5244050bbdfd370b
canggara1 <canggara1@gmail.com>
2025-05-12 01:37:36 +0700

    Add backend/routes/report_routes.py 1746988656201

--- backend/routes/report_routes.py
+++ backend/routes/report_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request, jsonify
-from backend.models import db, Transaction, Product, Transfer
-from backend.utils import token_required
+from models import db, Transaction, Product, Transfer
+from utils import token_required
 from datetime import datetime, timedelta
 from sqlalchemy import func
 

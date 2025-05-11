a0f3f9eb2b3351cd1cf35698a3b8912c9723ee78
canggara1 <canggara1@gmail.com>
2025-05-12 01:37:38 +0700

    Add backend/routes/transfer_routes.py 1746988658339

--- backend/routes/transfer_routes.py
+++ backend/routes/transfer_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request, jsonify
-from backend.models import db, Transfer, Product
-from backend.utils import token_required
+from models import db, Transfer, Product
+from utils import token_required
 from datetime import datetime
 
 bp = Blueprint('transfers', __name__)

95004c2d543e85b51bebb8c79d5a6497b02f5daa
canggara1 <canggara1@gmail.com>
2025-05-12 01:37:37 +0700

    Add backend/routes/pos_routes.py 1746988657211

--- backend/routes/pos_routes.py
+++ backend/routes/pos_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request, jsonify
-from backend.models import db, Transaction, Product
-from backend.utils import token_required
+from models import db, Transaction, Product
+from utils import token_required
 from datetime import datetime
 
 bp = Blueprint('pos', __name__)
